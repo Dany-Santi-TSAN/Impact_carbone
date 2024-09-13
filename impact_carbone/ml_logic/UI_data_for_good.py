aa0d77e537a42a33b1adc7b3df2a115d5d519a7f
Celian Baudet <changeup.celian@gmail.com>
2024-09-13 11:48:40 +0200

    Chagement de structure

--- impact_carbone/ml_logic/UI_data_for_good.py
+++ impact_carbone/ml_logic/UI_data_for_good.py
@@ -7,7 +7,7 @@ energy_source = None
 
 if 'submitted' not in st.session_state:
     st.session_state.submitted = False
-        
+
 def OnClickSubmit():
     st.session_state.submitted = True
 
@@ -22,7 +22,7 @@ with tab_physical:
     body_type = st.selectbox('Pick your body type.', ['normal', 'underweight', 'overweight', 'obese'])
     gender = st.selectbox('What is your gender?', ['female', 'male'])
     diet = st.selectbox('What kind of diet are you on?', ['omnivore', 'vegan', 'vegetarian', 'pescatarian'])
-    
+
 with tab_home:
     waste_bag_weekly_count = st.number_input('What is your weekly waste bag count?', min_value=1, step=1, format="%i")
     waste_bag_type = st.selectbox('How large are your waste bags?', ['large', 'extra large', 'small', 'medium'])
@@ -31,14 +31,14 @@ with tab_home:
     heating = st.selectbox('On what type of energy is your home heating running on?', ['coal', 'natural gas', 'wood', 'electricity'])
     cooking = st.multiselect('What are you cooking with?', ['Stove', 'Oven', 'Microwave', 'Grill', 'Airfryer'])
     efficiency = st.selectbox('Do you try to minimize your energy consumption?', ['No', 'Sometimes', 'Yes'])
-    
+
 with tab_transportation:
     transportation_mode = st.selectbox('What is your main mode of transportation?', ['public transport', 'walk/bicycle', 'private'])
     if transportation_mode == 'private':
         energy_source = st.selectbox('What is your vehicule energy source?', ['petrol', 'diesel', 'hybrid', 'lpg', 'electric'])
     monthly_distance = st.number_input('What is the average monthly distance you travel?', min_value=1, step=1, format="%i")
     air_travel = st.selectbox('How often do you travel by airplane?', ['frequently', 'rarely', 'never', 'very frequently'])
-    
+
 with tab_consumer_habit:
     social_activity = st.selectbox('How often do you partake in social activity?', ['often', 'never', 'sometimes'])
     monthly_grocery = st.number_input('What is your average monthly  grocery bill?', min_value=1, step=1, format="%i")
@@ -65,11 +65,11 @@ if st.session_state.submitted:
         "Energy_efficiency": [efficiency],
         "Sex": [gender],
         "Heating_Energy_Source": [heating],
-        "Transport_Vehicle_Type": [transportation_mode if transportation_mode != 'private' else f'car (type: {energy_source})'],        
+        "Transport_Vehicle_Type": [transportation_mode if transportation_mode != 'private' else f'car (type: {energy_source})'],
         "Cooking_With": [str(cooking)],
         "Recycling": [str(recycling)],
     })
-    
+
     # Chemin du modèle sauvegardé
     model_path = 'model_gbr.pkl'
 
@@ -81,5 +81,5 @@ if st.session_state.submitted:
     X_transformed_new = cf.transform(x_new)
     y_pred_new = predict_x(x_new, cf, best_gbr)
 
-    result = f"{round((y_pred_new[0] / 1_000), 2)} tonnes" if y_pred_new > 1_000 else f"{round(y_pred_new[0], 2)} kilogrammes"    
+    result = f"{round((y_pred_new[0] / 1_000), 2)} tonnes" if y_pred_new > 1_000 else f"{round(y_pred_new[0], 2)} kilogrammes"
     f"Votre score de pollution est de {result} de CO₂ par mois."
