2fb152af75d73889ed8d2fd9828989880dc90124
Benjamin Israël <benjamin.israel.lewagon@gmail.com>
2024-09-16 17:09:26 +0200

    - Déplace les fichiers de l'interface utilisateur depuis le dossier 'ml_logic' vers le dossier 'interface' - Améliore la lisibilité du formulaire - Ajoute une image de fond à l'interface

---
+++ impact_carbone/interface/ui_files/ui_functions.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+import ui_files.constants as constants
+
+def BuildFormDataframe(session_dict):
+    x_new = pd.DataFrame({
+        "Monthly_Grocery_Bill": [session_dict.monthly_grocery],
+        "Vehicle_Monthly_Distance_Km": [session_dict.monthly_distance],
+        "Waste_Bag_Weekly_Count": [session_dict.waste_bag_weekly_count],
+        "How_Long_TV_PC_Daily_Hour": [session_dict.screen_time],
+        "How_Many_New_Clothes_Monthly": [session_dict.clothes],
+        "How_Long_Internet_Daily_Hour": [session_dict.internet_time],
+        "Body_Type": [constants.DICT_DISPLAY_TO_MDL['body_type'][1][constants.DICT_DISPLAY_TO_MDL['body_type'][0].index(session_dict.body_type)]],
+        "Diet": [constants.DICT_DISPLAY_TO_MDL['diet'][1][constants.DICT_DISPLAY_TO_MDL['diet'][0].index(session_dict.diet)]],
+        "How_Often_Shower": [constants.DICT_DISPLAY_TO_MDL['personal_hygiene'][1][constants.DICT_DISPLAY_TO_MDL['personal_hygiene'][0].index(session_dict.personal_hygiene)]],
+        "Social_Activity": [session_dict.social_activity],
+        "Frequency_of_Traveling_by_Air": [constants.DICT_DISPLAY_TO_MDL['air_travel'][1][constants.DICT_DISPLAY_TO_MDL['air_travel'][0].index(session_dict.air_travel)]],
+        "Waste_Bag_Size" : [constants.DICT_DISPLAY_TO_MDL['waste_bag_type'][1][constants.DICT_DISPLAY_TO_MDL['waste_bag_type'][0].index(session_dict.waste_bag_type)]],
+        "Energy_efficiency": [constants.DICT_DISPLAY_TO_MDL['efficiency'][1][constants.DICT_DISPLAY_TO_MDL['efficiency'][0].index(session_dict.efficiency)]],
+        "Sex": [constants.DICT_DISPLAY_TO_MDL['gender'][1][constants.DICT_DISPLAY_TO_MDL['gender'][0].index(session_dict.gender)]],
+        "Heating_Energy_Source": [constants.DICT_DISPLAY_TO_MDL['heating'][1][constants.DICT_DISPLAY_TO_MDL['heating'][0].index(session_dict.heating)]],
+        "Transport_Vehicle_Type": [
+                constants.DICT_DISPLAY_TO_MDL['transportation_mode'][1][constants.DICT_DISPLAY_TO_MDL['transportation_mode'][0].index(session_dict.transportation_mode)] \
+                if session_dict.transportation_mode != constants.DICT_DISPLAY_TO_MDL['transportation_mode'][1][-1] else \
+                f"car (type: {constants.DICT_DISPLAY_TO_MDL['energy_source'][1][session_dict.energy_source]})"
+            ],
+        "Cooking_With": [str([constants.DICT_DISPLAY_TO_MDL['cooking'][1][constants.DICT_DISPLAY_TO_MDL['cooking'][0].index(choice)] for choice in session_dict.cooking])],
+        "Recycling": [str([constants.DICT_DISPLAY_TO_MDL['recycling'][1][constants.DICT_DISPLAY_TO_MDL['recycling'][0].index(choice)] for choice in session_dict.recycling])],
+    })
+    
+    return x_new
--- impact_carbone/ml_logic/predict.py
+++ impact_carbone/ml_logic/predict.py
@@ -1,19 +1,22 @@
 # Import de tout ce dont j'ai besoin
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-from preprocessing import preprocess, selection_types_features
-from data import data_cleaning_import
-from model import train_model
-import pandas as pd
-import numpy as np
-import seaborn as sns
+#from sklearn.ensemble import GradientBoostingRegressor
+#from sklearn.metrics import mean_absolute_error
+#from sklearn.model_selection import train_test_split
+#from preprocessing import preprocess, selection_types_features
+#from data import data_cleaning_import
+#from model import train_model
+#import pandas as pd
+#import numpy as np
+#import seaborn as sns
+from os import sep
+from os.path import realpath
+import pickle
 
 # Fonction pour prédire les données du streamlit
 def predict_x(x_new,cf,best_gbr) :
     # Selection des features sur x_new
-    variables_quantitative, variables_ordinal,variables_for_one_hot_encoded = selection_types_features(x_new)
-    variables_for_one_hot_encoded
+    #variables_quantitative, variables_ordinal,variables_for_one_hot_encoded = selection_types_features(x_new)
+    #variables_for_one_hot_encoded
 
     # Preprocessing sur x_new
     X_transformed_new = cf.transform(x_new)
@@ -21,4 +24,14 @@ def predict_x(x_new,cf,best_gbr) :
 
     # Prédictions avec le modèle entraîné
     y_pred_new = best_gbr.predict(X_transformed_new)
+    return y_pred_new
+
+def GetPrediction(x_new):
+    path_parts = realpath(__file__).split("/")
+    path_parts = path_parts[:-2] + ["ml_logic", "model.pkl"]
+    model_path = sep.join(path_parts)
+    # Charger le modèle avec pickle
+    with open(model_path, 'rb') as model_file:
+        best_gbr,cf, X_train, X_test, y_train, y_test = pickle.load(model_file)
+    y_pred_new = predict_x(x_new, cf, best_gbr)
     return y_pred_new
